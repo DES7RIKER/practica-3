f4fb000a69eb3c6d82a30641d76db7d1f5e32efa
Elias <eGasparArellano@gmail.com>
2018-05-14 15:11:17 -0500

    Comentarios sobre programa de prueba

--- PruebaSaltos_HzdControl.asm
+++ PruebaSaltos_HzdControl.asm
@@ -4,7 +4,7 @@
 	beq $zero, $zero, salto1
 	addi $t2, $zero, 4
 salto1: 
-	add $zero, $zero, $zero
+	add $zero, $zero, $zero#Nop
 	bne $v0, $zero, salto2
 	addi $t2, $zero, 5
 	
@@ -19,7 +19,7 @@ salto3:
 	
 function:
 	addi $v0, $zero, 88
-	add $zero, $zero, $zero
+	add $zero, $zero, $zero#Nop
 	jr $ra
 	
 exit:	
@@ -31,7 +31,7 @@ exit:
 	add $t2, $t0, $t1#20
 	sub $t2, $t1, $t2#-5
 	add $t3, $t2,$t2#-10	
-	add $zero, $zero, $zero
-	add $zero, $zero, $zero
-	add $zero, $zero, $zero
+	add $zero, $zero, $zero#Nop
+	add $zero, $zero, $zero#Nop
+	add $zero, $zero, $zero#Nop
 
